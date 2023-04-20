7c1cbf499495d132caf202c3e3b2d8960df2f6e5
nicolaao <nicolaao@stud.ntnu.no>
2023-04-20 14:00:07 +0200

    added pretty print encoder

---
+++ json_coder/encoder.go
@@ -0,0 +1,23 @@
+package json_coder
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+/*
+PrettyPrint gotten from 02-JSON-demo
+Using an interface so that no extra method is needed.
+*/
+func PrettyPrint(w http.ResponseWriter, in interface{}) {
+
+	output, err := json.MarshalIndent(in, "", "  ")
+	if err != nil {
+		log.Println("Error during pretty printing of output: " + err.Error())
+		http.Error(w, "Error during pretty printing", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, string(output))
+}
