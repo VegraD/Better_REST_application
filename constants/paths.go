a4cbd70cdd068a38f952af0aca5d886d78aeeefd
nicolaao <nicolaao@stud.ntnu.no>
2023-04-20 19:44:09 +0200

    Added necessary structs for currentHandler to work as intended.

--- constants/paths.go
+++ constants/paths.go
@@ -15,22 +15,28 @@ const (
 	DefaultEP string = "/"
 
 	// CurrentEP is the endpoint path for retrieving current renewable energy production.
-	CurrentEP string = renewablesEP + "/current"
+	CurrentEP string = renewablesEP + "/current/"
 
 	// HistoryEP is the endpoint path for retrieving historical renewable energy production.
-	HistoryEP string = renewablesEP + "/history"
+	HistoryEP string = renewablesEP + "/history/"
 
 	// NotificationsEP is the endpoint path for retrieving notifications.
-	NotificationsEP string = baseEP + "/notifications"
+	NotificationsEP string = baseEP + "/notifications/"
 
 	// StatusEP is the endpoint path for retrieving the status of the application.
-	StatusEP string = baseEP + "/status"
+	StatusEP string = baseEP + "/status/"
 )
 
 // External API paths
 const (
 	// CountryApi is the endpoint path for retrieving country information.
 	CountryApi = "http://129.241.150.113:8080/v3.1"
+
+	// coutryFullText is the query paramter to limit country results to full text matches.
+	CountryFullText = "?fullText=true"
+
+	// CountryAlpha it the subdirectory to search for country based on alpha code
+	CountryAlpha = "/alpha/"
 
 	// RenewablesApi is the endpoint path for retrieving renewable energy production information.
 	RenewablesApi = "https://drive.google.com/file/d/18G470pU2NRniDfAYJ27XgHyrWOThP__p/view"
