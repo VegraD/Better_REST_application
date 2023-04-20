95583a2257ba83adf682dd4ca24c293ea391c4c3
nicolaao <nicolaao@stud.ntnu.no>
2023-04-20 10:27:46 +0200

    added initial constants

---
+++ constants/constants.go
@@ -0,0 +1,16 @@
+package constants
+
+// Programs version as constant
+const VERSION = "v1"
+
+// Url Paths as constants
+const COUNTRY_API = "http://129.241.150.113:8080/v3.1"
+const RENEWABLES_API = "https://drive.google.com/file/d/18G470pU2NRniDfAYJ27XgHyrWOThP__p/view"
+
+// api Paths as constants
+const DEFAULT_PATH = "/energy/v1"
+const RENEWABLES_PATH = "/energy/v1/renewables"
+const CURRENT_PATH = "/current"
+const HISTORY_PATH = "/history"
+const NOTIFICATION_PATH = "/notification/"
+const STATUS_PATH = "/status/"
