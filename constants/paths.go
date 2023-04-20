7bb139323cd32472d55ca88793ced67dbd9bc253
nicolaao <nicolaao@stud.ntnu.no>
2023-04-20 10:42:54 +0200

    updated comments for the endpoints

--- constants/paths.go
+++ constants/paths.go
@@ -4,11 +4,21 @@ Package constants contains all constants used in the application. This includes 
 package constants
 
 // TODO: ?maybe? add comments to explain what each endpoint is used for as it's done for the default port
+// TODO: Sånn her du tenker? -Nicolai
 // Endpoint paths
 const (
-	DefaultEndpoint       string = "/"
-	CurrentEndpoint              = DEFAULT_PATH + RENEWABLES_PATH + CURRENT_PATH
-	HistoryEndpoint              = DEFAULT_PATH + RENEWABLES_PATH + HISTORY_PATH
-	NotificationsEndpoint        = DEFAULT_PATH + NOTIFICATION_PATH
-	StatusEndpoint               = DEFAULT_PATH + STATUS_PATH
+	// DefaultEndpoint is the default endpoint for the application.
+	DefaultEndpoint string = "/"
+
+	// currentEndpoint is the endpoint for the current renewable energy production for the given country
+	CurrentEndpoint = DEFAULT_PATH + RENEWABLES_PATH + CURRENT_PATH
+
+	// HistoryEndpoint is the endpoint for the history of renewable energy production for the given country
+	HistoryEndpoint = DEFAULT_PATH + RENEWABLES_PATH + HISTORY_PATH
+
+	// NotificationsEndpoint is the endpoint for the notifications for the given country
+	NotificationsEndpoint = DEFAULT_PATH + NOTIFICATION_PATH
+
+	// StatusEndpoint is the endpoint for the status of the api
+	StatusEndpoint = DEFAULT_PATH + STATUS_PATH
 )
