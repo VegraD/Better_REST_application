4f5c55baf4ef49d8d330e21c85e26a125eaad7b3
Joachim <joachiom@stud.ntnu.no>
2023-04-21 16:24:15 +0200

    Added an internal path to the CSV file and comments for the constants.

--- constants/internalPaths.go
+++ constants/internalPaths.go
@@ -1,5 +1,9 @@
 package constants
 
 const (
+	// DefaultHtml is the path to the defaultEP html file.
 	DefaultHtml = "static/html/defEndpoint.html"
+
+	// HistoricalCsv is the path to the historical renewable energy production csv file.
+	HistoricalCsv = "res/renewable-share-energy.csv"
 )
