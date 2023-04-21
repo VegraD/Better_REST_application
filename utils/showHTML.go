d97a1b9a25a839cae770fe625a04253540c07029
Joachim <joachiom@stud.ntnu.no>
2023-04-21 02:33:09 +0200

    Added logging to error handling.

--- utils/showHTML.go
+++ utils/showHTML.go
@@ -15,8 +15,10 @@ func DisplayHTML(w http.ResponseWriter, filePath string) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		if os.IsNotExist(err) { // If file does not exist
+			log.Println("File does not exist: " + filePath)
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		} else { // If file exists but another error occurred
+			log.Printf("Failed to open file: %s", err)
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		}
 		return
@@ -33,6 +35,7 @@ func DisplayHTML(w http.ResponseWriter, filePath string) {
 	// Copy file contents to response writer
 	_, err = io.Copy(w, file)
 	if err != nil {
+		log.Printf("Failed to copy file contents to response writer: %s", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
