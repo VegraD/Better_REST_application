3914832bfa826776d59bed3ddc597726ecba59c0
Joachim <joachiom@stud.ntnu.no>
2023-04-23 17:08:48 +0200

    Added the structure for notificationHandler() and added placeholder cases.

--- handlers/notificationHandler/notificationHandler.go
+++ handlers/notificationHandler/notificationHandler.go
@@ -1,11 +1,19 @@
 package notificationHandler
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func notifHandler(w http.ResponseWriter, r http.Response) {
-
-	//part := strings.Split(r.URL.Path, "/")
-
+func NotificationHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Println("Get request")
+	case http.MethodPost:
+		fmt.Println("Post request")
+	case http.MethodDelete:
+		fmt.Println("Delete request")
+	default:
+		http.Error(w, r.Method+" not supported, use "+http.MethodGet+"!", http.StatusMethodNotAllowed)
+	}
 }
