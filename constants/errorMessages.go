1dd8598f6c505f3b3c55f3edcaf4c1e60d2e8b02
Joachim <joachiom@stud.ntnu.no>
2023-04-23 03:43:40 +0200

    Implemented the moved OpenFile function. Removed errorMessage constants file.

--- constants/errorMessages.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const (
-	// CloseFileFail Is shown when the file could not be closed.
-	CloseFileFail string = "failed to close file: "
-
-	// OpenFileFail Is shown when the file could not be opened.
-	OpenFileFail string = "failed to open file: "
-)
--- utils/showHTML.go
+++ utils/showHTML.go
@@ -4,24 +4,18 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
 )
 
+// DisplayHTML is used for displaying HTML files.
 func DisplayHTML(w http.ResponseWriter, filePath string) {
 	// Set content type to html
 	w.Header().Set("Content-Type", "text/html")
 
-	// TODO: Put the opening into a separate function in the /utils/fileHandler.go file?
 	// Open the file
-	file, err := os.Open(filePath)
+	file, err := OpenFile(filePath)
 	if err != nil {
-		if os.IsNotExist(err) { // If file does not exist
-			log.Println("File does not exist: " + filePath)
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		} else { // If file exists but another error occurred
-			log.Printf("Failed to open file: %s", err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		}
+		log.Printf("Failed to open file: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
