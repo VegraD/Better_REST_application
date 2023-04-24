41df2b138b237e74cd5f20cd426a2bfe931ea9ba
Nicolaao <nicolaao@stud.ntnu.no>
2023-04-24 16:22:38 +0200

    Removed unecesary field in struct

--- structs/structs.go
+++ structs/structs.go
@@ -38,8 +38,7 @@ type URLParams struct {
 }
 
 type Border struct {
-	Landlocked bool     `json:"landlocked"`
-	Borders    []string `json:"borders"`
+	Borders []string `json:"borders"`
 }
 
 // ################################################## Webhook structs ##################################################
