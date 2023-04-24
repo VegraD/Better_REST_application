4e1cccf83ed86aefe6a35e557c2d785ee2cc923d
Joachim <joachiom@stud.ntnu.no>
2023-04-24 16:03:33 +0200

    Added a field (Neighbours) to the URLParams struct so that it will work for currentHandler.

--- structs/structs.go
+++ structs/structs.go
@@ -35,6 +35,7 @@ type URLParams struct {
 	BeginYear   string
 	EndYear     string
 	SortByValue bool
+	Neighbours  bool
 }
 
 // ################################################## Webhook structs ##################################################
