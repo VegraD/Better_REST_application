98f3e48f48b5d7bdc6fcd4773e62b2fe2cc58def
Vegard Johansen <vejo@stud.ntnu.no>
2023-04-26 18:29:43 +0200

    Added function for deleting entry from database

--- database/firestoreWebhooksDB.go
+++ database/firestoreWebhooksDB.go
@@ -121,3 +121,26 @@ func UpdateWebhooks(url string, country string, noCalls int, count int) (string,
 		return webhookId, nil
 	}
 }
+
+func DeleteWebhooks(id string) error {
+
+	response := client.Collection(collection).Doc(id)
+
+	_, err := response.Get(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = response.Delete(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
