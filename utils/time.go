ffe9f51f597fd0891f920effe69d4b724cf42443
Nicolaao <nicolaao@stud.ntnu.no>
2023-04-27 00:04:16 +0200

    Updated Uptime function to return time in seconds only.

--- utils/time.go
+++ utils/time.go
@@ -10,15 +10,15 @@ var StartTime = time.Now()
 /*
 Uptime returns the uptime of the server
 */
-func Uptime() string {
+func Uptime() int {
 	// Get the time since the server started
-	runtime := time.Since(StartTime).Seconds()
+	runtime := int(time.Since(StartTime).Seconds())
 
 	// Convert the time to a string with 2 decimals
 	//uptime := strconv.FormatFloat(runtime, 'f', 2, 64)
 
 	// Convert the time to hours, minutes and seconds. E.g., 1h 2m 3s and round to 2 decimals
-	uptime := time.Duration(runtime * float64(time.Second)).Round(time.Second).String()
+	//uptime := time.Duration(runtime * float64(time.Second)).Round(time.Second).String()
 
-	return uptime
+	return runtime
 }
