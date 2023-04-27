fb06bf4b3dd7d1e927087cffab8b7680c7e6f792
Joachim <joachiom@stud.ntnu.no>
2023-04-27 13:30:10 +0200

    Created an additional test for GetDefault that checks the http status and a test for checking of the body of the response is as expected.

--- handlers/defaultHandler/defaultHandler_test.go
+++ handlers/defaultHandler/defaultHandler_test.go
@@ -1,37 +1,115 @@
 package defaultHandler
 
 import (
+	"assignment-2/constants"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
 
-// Test function for default endpoint.
-func TestDefaultHandler(t *testing.T) {
+// Test cases for testing the default endpoint, these are static and will not change therefore declared here.
+var testCases = []struct {
+	name string
+	path string
+}{
+	{name: "DefaultEP", path: constants.DefaultEP},
+	{name: "BasePath", path: constants.BasePath},
+	{name: "BasePath + DefaultEP", path: constants.BasePath + constants.DefaultEP},
+}
+
+func TestMain(m *testing.M) {
+	// Get current working dir.
 	wd, err := os.Getwd()
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	t.Logf("Current working directory: %s", wd)
-
-	err = os.Chdir("..\\..\\")
-	if err != nil {
-		t.Fatal(err)
+	// If the current working dir is not the top lvl, change it.
+	if wd[len(wd)-3:] != "assignment-2" {
+		err = os.Chdir("..\\..\\")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+	// Run tests
+	m.Run()
 
-	req, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
+}
+
+// TestDefaultHandler tests that the http status code is 200 when the default endpoint is called.
+func TestDefaultHandler(t *testing.T) {
+
+	// Run tests
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, tc.path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(DefaultHandler)
+
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+		})
 	}
+}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(DefaultHandler)
+// TestGetDefault tests that the http status code is 200 when the default endpoint is called.
+func TestGetDefault(t *testing.T) {
 
-	handler.ServeHTTP(rr, req)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, tc.path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(getDefault)
+
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+		})
+	}
+}
+
+// TestGetDefaultBody tests that the body of the response is the same as the default html file.
+func TestGetDefaultBody(t *testing.T) {
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, tc.path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(getDefault)
+
+			handler.ServeHTTP(rr, req)
+
+			// Get the content of the defEndpoint.html file as a string.
+			defaultHtml, err := os.ReadFile(constants.DefaultHtml)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Checks that the body of the response is the same as the contents of the default html file.
+			if rr.Body.String() != string(defaultHtml) {
+				t.Errorf("handler returned wrong body: got %v want %v",
+					rr.Body.String(), string(defaultHtml))
+			}
+		})
 	}
 }
