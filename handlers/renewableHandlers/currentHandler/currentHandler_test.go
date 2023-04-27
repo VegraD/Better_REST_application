5a2a420bd0f35ddb2f53681265dc167bbe063a4e
Nicolaao <nicolaao@stud.ntnu.no>
2023-04-27 14:02:59 +0200

    Fixed problem with test in currentHandler_test.go

--- handlers/renewableHandlers/currentHandler/currentHandler_test.go
+++ handlers/renewableHandlers/currentHandler/currentHandler_test.go
@@ -5,6 +5,7 @@ import (
 	"assignment-2/database"
 	"assignment-2/structs"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -38,20 +39,10 @@ func TestMain(m *testing.M) {
 	server := httptest.NewServer(http.HandlerFunc(CurrentHandler))
 	defer server.Close()
 
-	//borders, err := utils.GetBordersFromJson()
-	//records, err := utils.GetCountriesFromCsv()
-	//if err != nil {
-	//	return
-	//}
+	currentEndpoint = httptest.NewServer(http.HandlerFunc(CurrentHandler))
+	defer currentEndpoint.Close()
 
-	//client := http.Client{}
-
-	// URL under which server is instantiated
-
-	//res, err := client.Get(server.URL + constants.CurrentEP)
-	//if err != nil {
-	//	t.Fatal("Get request to URL failed:", err.Error())
-	//}
+	defer database.ClearDB()
 
 	m.Run()
 
@@ -67,92 +58,80 @@ func TestSingleCountry(t *testing.T) {
 		args               args
 		method             string
 		expectedStatusCode int
-		excpectedResponse  structs.CountryInfo
+		expectedResponse   []structs.CountryInfo
 	}{
 		{
 			name: "Valid Get Request to Current Endpoint with Country as ISO Code",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NOR",
+				url: currentEndpoint.URL + constants.CurrentEP + "NOR",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
-			excpectedResponse: structs.CountryInfo{
-				Country:    "Norway",
-				IsoCode:    "NOR",
-				Year:       2021,
-				Percentage: 71.558365,
-			},
+			expectedResponse: []structs.CountryInfo{{Country: "Norway", IsoCode: "NOR", Year: 2021,
+				Percentage: 71.558365}},
 		},
 		{
 			name: "Valid Get Request to Current Endpoint with Country as case-mixed ISO Code",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NoR",
+				url: currentEndpoint.URL + constants.CurrentEP + "NoR",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
-			excpectedResponse: structs.CountryInfo{
-				Country:    "Norway",
-				IsoCode:    "NOR",
-				Year:       2021,
-				Percentage: 71.558365,
-			},
+			expectedResponse: []structs.CountryInfo{{Country: "Norway", IsoCode: "NOR", Year: 2021,
+				Percentage: 71.558365}},
 		}, {
 			name: "Valid Get Request to Current Endpoint with full country name",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/Norway",
+				url: currentEndpoint.URL + constants.CurrentEP + "Norway",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
-			excpectedResponse: structs.CountryInfo{
-				Country:    "Norway",
-				IsoCode:    "NOR",
-				Year:       2021,
-				Percentage: 71.558365,
-			},
+			expectedResponse: []structs.CountryInfo{{Country: "Norway", IsoCode: "NOR", Year: 2021,
+				Percentage: 71.558365}},
 		}, {
 			name: "Valid Get Request to Current Endpoint with case-mixed full country name",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NoRWaY",
+				url: currentEndpoint.URL + constants.CurrentEP + "NoRWaY",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
-			excpectedResponse: structs.CountryInfo{
-				Country:    "Norway",
-				IsoCode:    "NOR",
-				Year:       2021,
-				Percentage: 71.558365,
-			},
+			expectedResponse: []structs.CountryInfo{{Country: "Norway", IsoCode: "NOR", Year: 2021,
+				Percentage: 71.558365}},
 		}, {
 			name: "Invalid Post Request",
 			args: args{
-				url: currentEndpoint.URL + "/corona/v1/cases/Norway",
+				url: currentEndpoint.URL + "/NOR",
 			},
 			method:             http.MethodPost,
-			expectedStatusCode: http.StatusMethodNotAllowed,
+			expectedStatusCode: 501,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client := http.Client{}
 			req, err := http.NewRequest(tt.method, tt.args.url, nil)
 			if err != nil {
-				t.Fatal("Failed to create request:", err.Error())
+				t.Fatal(err)
 			}
-			res, err := client.Do(req)
+			res, err := http.DefaultClient.Do(req)
 			if err != nil {
-				t.Fatal("Get request to URL failed:", err.Error())
+				t.Fatal(err)
 			}
-			if res.StatusCode != tt.expectedStatusCode {
-				t.Errorf("Expected status code %v, got %v", tt.expectedStatusCode, res.StatusCode)
-			}
-			if res.StatusCode == http.StatusOK {
-				var actualResponse structs.CountryInfo
-				err = json.NewDecoder(res.Body).Decode(&actualResponse)
+			defer func(Body io.ReadCloser) {
+				err := Body.Close()
 				if err != nil {
-					t.Fatal("Failed to decode response:", err.Error())
+					t.Fatal(err)
 				}
-				if !reflect.DeepEqual(actualResponse, tt.excpectedResponse) {
-					t.Errorf("Expected response %v, got %v", tt.excpectedResponse, actualResponse)
+			}(res.Body)
+			if res.StatusCode != tt.expectedStatusCode {
+				t.Errorf("expected status code %v, got %v", tt.expectedStatusCode, res.StatusCode)
+			} else if res.StatusCode == http.StatusOK {
+				var got []structs.CountryInfo
+				err = json.NewDecoder(res.Body).Decode(&got)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !reflect.DeepEqual(got, tt.expectedResponse) {
+					t.Errorf("expected response %v, got %v", tt.expectedResponse, got)
 				}
 			}
 		})
@@ -173,7 +152,7 @@ func TestNeighbours(t *testing.T) {
 		{
 			name: "Valid Get Request to Current Endpoint with Country as ISO Code and neighbours set to true",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NOR?neighbours=true",
+				url: currentEndpoint.URL + constants.CurrentEP + "NOR?neighbours=true",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
@@ -207,7 +186,7 @@ func TestNeighbours(t *testing.T) {
 			name: "Valid Get Request to Current Endpoint with Country as mixed-case ISO Code and neighbours set " +
 				"to true",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NoR?neighbours=true",
+				url: currentEndpoint.URL + constants.CurrentEP + "NoR?neighbours=true",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
@@ -241,7 +220,7 @@ func TestNeighbours(t *testing.T) {
 			name: "Valid Get Request to Current Endpoint with Country as ISO Code and neighbours set to true in path " +
 				"format",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NOR/true",
+				url: currentEndpoint.URL + constants.CurrentEP + "NOR/true",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
@@ -275,7 +254,7 @@ func TestNeighbours(t *testing.T) {
 			name: "Valid Get Request to Current Endpoint with Country as mixed-case ISO Code and neighbours set to " +
 				"mixed-case true in path format",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NOR/true",
+				url: currentEndpoint.URL + constants.CurrentEP + "NOR/true",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
@@ -310,7 +289,7 @@ func TestNeighbours(t *testing.T) {
 			name: "Valid Get Request to Current Endpoint with Country as mixed-case Full Name and neighbours set to " +
 				"mixed-case true in path format",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NORwaY/true",
+				url: currentEndpoint.URL + constants.CurrentEP + "NORwaY/true",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
@@ -344,7 +323,7 @@ func TestNeighbours(t *testing.T) {
 			name: "Valid Get Request to Current Endpoint with Country as mixed-case full country name and neighbours " +
 				"set to true",
 			args: args{
-				url: currentEndpoint.URL + constants.CurrentEP + "/NoRWaY?neighbours=trUe",
+				url: currentEndpoint.URL + constants.CurrentEP + "NoRWaY?neighbours=true",
 			},
 			method:             http.MethodGet,
 			expectedStatusCode: http.StatusOK,
@@ -386,7 +365,12 @@ func TestNeighbours(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			defer res.Body.Close()
+			defer func(Body io.ReadCloser) {
+				err := Body.Close()
+				if err != nil {
+					t.Fatal(err)
+				}
+			}(res.Body)
 			if res.StatusCode != tt.expectedStatusCode {
 				t.Errorf("expected status code %v, got %v", tt.expectedStatusCode, res.StatusCode)
 			}
